0b40e9380bb58717f75120e8e5731e4b5c66aa7d
Mahitee Satasiya <mahitee.satasiya@sjsu.edu>
2019-09-27 19:51:38 -0700

    Longest Increasing Subsequence DP

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,25 @@
+package dp9;
+
+import java.util.Arrays;
+
+// TC: O(n^2)
+public class LongestIncreasingSubsequence {
+	public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        
+        Arrays.fill(dp, 1);
+        
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for(int i = 0; i < nums.length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
