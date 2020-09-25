287edd9332b04050781c6ebec7a57a74544fda04
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-09-25 16:59:49 -0500

    Longest Increasing subsequence

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -1,11 +1,45 @@
+/****************************Using DP***********************************/
+// Time Complexity :O(n^2), n is length of nums
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :no
 
+class Longest_Increasing_Subsequence_DP1 {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+        int[] dp = new int[nums.length];   
+        int max = 1;
+         //Initialize the Length of longest Increasing Subsequence(dp) values for all indexes as 1
+        for(int i=0; i<dp.length; i++)
+            dp[i] = 1;
+        
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    // it means i is forming a increasing subsequence with j
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }        
+        return max;
+    }
+}
+
+/**********************************************************************************************/
+//Time Complexity :O(n^2), n is length of nums
+//Space Complexity :O(n)
+//Did this code successfully run on Leetcode : yes
+//Any problem you faced while coding this :no
 class Longest_Increasing_Subsequence_DP {
 	public int lengthOfLIS(int[] nums) {
 		if(nums == null || nums.length == 0)
-			return nums.length;
+			return 0;
 
 		int[] dp = new int[nums.length];   
-		int max = 0;
+		int max = 1;
 		//Initialize the Length of longest Increasing Subsequence(dp) values for all indexes as 1
 		for(int i=0; i<dp.length; i++)
 			dp[i] = 1;
@@ -31,4 +65,47 @@ class Longest_Increasing_Subsequence_DP {
 		}
 		return max;
 	}
+}
+
+
+/****************************Using Binary Search***********************************/
+//Time Complexity :O(n log n), n is length of nums
+//Space Complexity :O(n)
+//Did this code successfully run on Leetcode : yes
+//Any problem you faced while coding this :no
+
+class Longest_Increasing_Subsequence_BS {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int[] arr = new int[nums.length];
+        int len= 0;
+        arr[len] = nums[0];
+        len++;
+        for(int i=1; i<nums.length; i++){
+            //case1
+            if(nums[i] > arr[len-1]){
+                arr[len] = nums[i];
+                len++;
+            }
+            else{
+                //input is at its correct index, replace the just greater element
+                int index = binarysearch(arr, 0, len, nums[i]);
+                arr[index] = nums[i];
+            }
+        }
+        return len;
+    }
+    private int binarysearch(int[] arr, int low, int high, int target){
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == target)
+                return mid;
+            if(arr[mid] > target)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return low; // low ptr is always at the just greater element
+    }
 }
