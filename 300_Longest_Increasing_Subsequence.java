308707234f8f3fd5d1cf41d73ffed4b40c14930c
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2020-11-06 19:49:47 -0800

    Solved Problems

---
+++ 300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+        int max = 1;
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp,1);
+        for(int i = 1;i < nums.length; i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    max = Math.max(max,dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
