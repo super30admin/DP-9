406df0b0bfba82784b5ad12d3b027d4bcbf7e16e
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-12-19 05:59:54 -0500

    Create Russian_doll.java

---
+++ Russian_doll.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0) return 0;
+        Arrays.sort(envelopes, (a,b) -> a[1] - b[1]);
+        
+        int [] dp = new int[envelopes.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        
+        for(int i = 1; i<envelopes.length; i++){
+            for(int j = 0; j < i; j++){
+                if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        
+        return max;
+    
+    }
+}
+
+
+//DP solution
+
