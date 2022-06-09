7954f8f1fb7d08fbce616b3a068ec44cc898009c
Renali01 <59395990+Renali01@users.noreply.github.com>
2022-06-08 22:42:20 -0400

    Done DP 9

---
+++ RussianDoll.java
@@ -0,0 +1,38 @@
+// O(N log N), N is length of input, both sorting array and finding longest inc subsequence happen in O(N log N)
+// O(N) space: lis function requires dp array which goes up to size N
+
+class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    public int maxEnvelopes(int[][] envelopes) {
+        // sort on increasing in first dimension and decreasing in second
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] arr1, int[] arr2) {
+                if (arr1[0] == arr2[0]) {
+                    return arr2[1] - arr1[1];
+                } else {
+                    return arr1[0] - arr2[0];
+                }
+           }
+        });
+        // extract the second dimension and run LIS
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; ++i) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+}
