0409f51fcbcc85d350467aca760ab197772c75e5
skatkar <skatkar225@gmail.com>
2022-06-22 18:10:48 -0500

    DP-9 submission

--- LongestSubsequence.java
+++ LongestSubsequence.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class LongestSubsequence {
+    // DP is not an optimal solution. The alternative method will be optimized one.
     // TC : O(n^2)
     // SC : O(n)
     public int lengthOfLIS(int[] nums) {
@@ -20,5 +21,41 @@ public class LongestSubsequence {
             }
         }
         return max;
+    }
+
+    // Optimized approach
+    public int lengthOfLIS2(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int len = 1;
+
+        for(int i=1; i < n; i++) {
+            if(nums[i] > dp[len-1]){
+                dp[len] = nums[i];
+                len++;
+            }else {
+                int searchIndex = binarySearch(dp, 0, len - 1, nums[i]);
+                dp[searchIndex] = nums[i];
+            }
+        }
+
+        return len;
+    }
+
+    private int binarySearch(int[] dp, int low, int high, int target) {
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(dp[mid] == target){
+                return mid;
+            }else if(target < dp[mid]) {
+                high = mid - 1;
+            }else
+                low = mid + 1;
+        }
+
+        return low;
     }
 }
