eaa1237bf63dfa8cbced0f493cbf09bfacce01ad
skatkar <skatkar225@gmail.com>
2022-06-22 17:19:33 -0500

    DP-9 submission

---
+++ LongestSybsequence.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LongestSybsequence {
+    // TC : O(n^2)
+    // SC : O(n)
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int max = 1;
+        for(int i=1; i < n;i++) {
+            for(int j=0; j < i;j ++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
