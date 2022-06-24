3c196a5e61e621100d44f198e424d45fa6c3402a
Rujal <rujal8452@gmail.com>
2022-06-24 07:49:55 -0400

    Problem1

--- Problem1.java
+++ Problem1.java
@@ -22,4 +22,43 @@ public class Problem1 {
         }
         return max;
     }
+
+    // DP  with O(n) time complexity solution
+    // TC : O(n^2)
+    // SC : O(n)
+    public int lengthOfLIS1(int[] nums) {
+
+        if (nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        int len = 1;
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (dp[len - 1] < nums[i]) {
+                dp[len] = nums[i];
+                len++;
+            } else {
+                int index = binaryIdx(dp, nums[i], 0, len - 1);
+                dp[index] = nums[i];
+            }
+        }
+        return len;
+    }
+
+    private int binaryIdx(int[] dp, int target, int low, int high) {
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (dp[mid] == target) {
+                return mid;
+            } else if (dp[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
 }
