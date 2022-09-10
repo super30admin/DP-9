842f79a92d0da1d23b59ef0321f08cf466ee5ece
prasannaOO7 <70802340+prasannaOO7@users.noreply.github.com>
2022-09-10 02:43:20 -0400

    Create longestincreasing.java

---
+++ longestincreasing.java
@@ -0,0 +1,23 @@
+// TC: O(n^2)
+// SC: O(n)
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        int n=nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        int max =1;
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    max = Math.max(max,dp[i]);
+                }
+            }
+        }
+        
+        return max;
+    }
+}
