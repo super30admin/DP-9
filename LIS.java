5206a0ca141f5f0686d5f9cfc2a9aa9534e83cca
Rishabh Mehta <rishabhsarangmehta@gmail.com>
2022-09-22 18:58:22 -0400

    Create LIS.java

---
+++ LIS.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+     
+        int []dp= new int[nums.length];
+        
+        
+        Arrays.fill(dp,1);
+        
+        int max=1;
+        
+        for( int i=1;i<nums.length;i++){
+        for(int j=0;j<i;j++){
+            
+            if(nums[j]<nums[i]){
+            
+                dp[i]=Math.max(dp[j]+1,dp[i]);
+                
+            }
+            
+            
+         max=Math.max(dp[i],max);   
+        }    
+            
+            
+        }
+      
+        return max;
+    }
+}
