75cbdb76415358cf427044e269c25d53fc806b05
Shivani31996 <shivani.chavan96@gmail.com>
2022-12-25 22:01:02 -0500

    Please review

--- LongestSub.java
+++ LongestSub.java
@@ -39,3 +39,55 @@ class Solution {
         return max;
     }
 }
+
+
+//This solution will give us the length of the longest subsequence only. Not the exact subsequence
+/*
+ * 1 - We build an effective array which works by the following way:
+ * - We add the first element to this array. IF the incoming element is greater than the last element, then we add it 
+ * to the array after the last element. If the element is not greater, then we find the net greater element than the element 
+ * in iteration and perform BS to replace this element in the right index.
+ */
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0 || nums == null)
+            return 0;
+        
+        int n = nums.length;
+        int[] arr = new int[n];
+        arr[0] = nums[0];
+        int len = 1;
+        
+        for(int i = 1; i<n; i++)
+        {
+            if(nums[i] > arr[len-1])
+            {
+                arr[len] = nums[i];
+                len++;
+            }
+            else
+            {
+                int binInd = binarySearch(arr,0,len-1,nums[i]);
+                arr[binInd] = nums[i];
+            }
+        }
+        return len;
+    }
+    
+    private int binarySearch(int[] arr, int low, int high, int target)
+    {
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if(arr[mid] == target)
+                return mid;
+            else if(arr[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+}
