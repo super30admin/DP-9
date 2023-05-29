d009f5e5310fc6149154f8e29f69ce9e79d0da4f
TrivediShaili <126596926+TrivediShaili@users.noreply.github.com>
2023-05-29 13:51:54 -0400

    Add files via upload

---
+++ Russian_Doll_Envelopes.py
@@ -0,0 +1,33 @@
+# Time Complexity : O(n log n), where n is the number of envelopes in the input list
+# Space Complexity :  O(n)
+from typing import List
+
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        n = len(envelopes)
+        if n == 0:
+            return 0
+        
+        # Sort the envelopes in ascending order of width and descending order of height
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        
+        # Use binary search to find the index where we can insert each envelope into a sorted list
+        dp = []
+        
+        for envelope in envelopes:
+            left, right = 0, len(dp) - 1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                if dp[mid][1] < envelope[1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            if left < len(dp):
+                dp[left] = envelope
+            else:
+                dp.append(envelope)
+        
+        return len(dp)
