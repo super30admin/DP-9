b2b5751f5947c64a0129affc808d804e8b098e1b
Nickil Vishwaa MS <nickilvishwaams@gmail.com>
2023-07-10 23:35:59 +0530

    Completed DP-9

---
+++ 300. Longest Increasing Subsequence.py
@@ -0,0 +1,21 @@
+#Time Complexity:O(n^2)
+#Space Complexity:O(n)
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+                    
+        return max(dp)
