d6bf27d174ac1187f6a2ed526277462f1e38ccc8
Shreya Agrawal <84276060+ag-sh1996@users.noreply.github.com>
2024-03-09 19:13:05 -0600

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,33 @@
+class Solution {
+    //TC: O(nlogn)
+    //SC: O(n)
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length;
+        int [] arr = new int[n];
+        Arrays.sort(envelopes,(a,b) ->{
+            if(a[0] == b[0]) return b[1] - a[1];
+            return a[0] - b[0];
+        });
+        int len = 1;
+        arr[0] = envelopes[0][1];
+        for(int i = 1; i < n; i++){
+            if(arr[len-1] < envelopes[i][1]){
+                arr[len] = envelopes[i][1];
+                len++;
+            }else{
+                int bstIndex = binarySearch(arr,0,len-1,envelopes[i][1]);
+                arr[bstIndex] = envelopes[i][1];
+            }
+        }
+        return len;
+    }
+    private int binarySearch(int [] arr, int low, int high, int target){
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
